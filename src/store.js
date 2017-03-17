ffc2d4cf5dfebd830b3b6843c658c7032056aaf4
Frank Gutierrez <dearfrankg@gmail.com>
2017-03-17 13:00:02 -0700

    refactor store

--- src/store.js
+++ src/store.js
@@ -4,8 +4,8 @@ class Game {
   static create = () => new Game()
   PLAYER1_SYMBOL = 'X'
   PLAYER2_SYMBOL = 'O'
-  currentPlayer = 'X'
   winningCombos = [ [0,1,2], [3,4,5], [6,7,8], [0,3,6], [1,4,7], [2,5,8], [0,4,8], [2,4,6] ]
+  currentPlayer = 'X'
   @observable cells = ['', '', '', '', '', '', '', '', '']
   @computed get winner () {
     const { PLAYER1_SYMBOL, PLAYER2_SYMBOL, winningCombos } = this
